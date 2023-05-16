7f2d19968fda2fc4d106511b7c4aa34d527181ac
NidhiIITB <104896142+NidhiIITB@users.noreply.github.com>
2023-05-16 09:22:11 +0530

    app.js

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
+const blogs = [
+{
+title: "My First Blog Post",
+content: "This is my first blog post. I'm excited to start blogging!"
+},
+{
+title: "My Second Blog Post",
+content: "This is my second blog post. I'm still excited to be blogging!"
+},
+{
+title: "My Third Blog Post",
+content: "This is my third blog post. I'm starting to get the hang of this blogging thing!"
+}
+];
 
+function getBlogs() {
+return blogs;
+}
+
+function renderBlogs(blogs) {
+const ul = document.querySelector("ul");
+blogs.forEach((blog) => {
+const li = document.createElement("li");
+li.textContent = blog.title;
+ul.appendChild(li);
+});
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+renderBlogs(getBlogs());
+});
